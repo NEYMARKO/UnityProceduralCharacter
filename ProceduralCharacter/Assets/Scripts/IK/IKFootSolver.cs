c24a2ff6285ddcdc7ac6dc83ce6599f2c8289746
NEYMARKO <marko.prosenjak5@gmail.com>
2024-11-24 13:50:29 +0100

    Cleaned all the failed code

--- ProceduralCharacter/Assets/Scripts/IK/IKFootSolver.cs
+++ ProceduralCharacter/Assets/Scripts/IK/IKFootSolver.cs
@@ -18,44 +18,42 @@ public class IKFootSolver : MonoBehaviour
     [SerializeField] ProceduralMovement proceduralMovement;
     [SerializeField] IKFootSolver otherFoot;
     [Header("Feet Parameters")]
+    [SerializeField] Transform footTransform;
     [SerializeField] float speed = 1f;
     [SerializeField] float stepDistance = 1f;
     [SerializeField] float stepLength = 1f;
     [SerializeField] float stepHeight = 1f;
     [SerializeField] float footHeightOffset = 0.1f;
-    [SerializeField] Vector3 kneePosition;
-    [SerializeField] Transform footTransform;
-    [SerializeField] float angleOffsetTolerance;
+    [SerializeField] float kneeHeightOffset;
     //foot side offset from the center of the body
     float footSpacing;
     float sphereCastRadius = 0.05f;
     Vector3 oldPosition, currentPosition, newPosition;
     Vector3 oldNormal, currentNormal, newNormal;
-    Vector3 enteringBodyForward = Vector3.zero;
     Transform body;
     float animationCompleted;
     bool previouslyMoved = false;
     RaycastHit hit, bodyAlignedHit, recoveryHit;
     Quaternion defaultToeRotation, footRotator = Quaternion.identity;
-    Quaternion lastPlayerRotation;
     [Header("Recovery")]
     [SerializeField] float recoverySpeed;
-    Vector3 enteringForward;
+    Vector3 previousForwardDirection;
+    Vector3 oldBodyPos;
+
     struct Plane
     {
         public Vector3 normal;
         public float D;
     }
-
     Plane intersectionPlane;
+    
     void Start()
     {
         footSpacing = transform.localPosition.x;
         body = proceduralMovement.transform;
 
         chainLength = CalculateIKChainLength();
-        //Debug.Log("CHAIN LENGTH: " + chainLength);
-        //STARTING POSITION
+        
         if (startingLeg)
         {
             transform.position = body.position + body.right * footSpacing - body.forward * stepLength;
@@ -67,42 +65,33 @@ public class IKFootSolver : MonoBehaviour
         }
         
         currentPosition = oldPosition = newPosition = transform.position;
-        currentNormal = oldNormal = newNormal = transform.up;
+        currentNormal = oldNormal = newNormal = Vector3.up;
+        oldBodyPos = body.position;
         animationCompleted = 1f;
 
         defaultToeRotation = footTransform.rotation;
-        footRotator = Quaternion.FromToRotation(Vector3.up, body.up) * Quaternion.FromToRotation(Vector3.forward, body.forward);
-        lastPlayerRotation = proceduralMovement.GetPlayerRotation();
     }
 
     void Update()
     {
-        //transform.position = !proceduralMovement.DetectedMovementInput() ? recoveryHit.point : currentPosition + newNormal * footHeightOffset;
         transform.position = currentPosition + footRotator * Vector3.up * footHeightOffset;
         UpdateBodyAlignedHit();
-        
+
         if (ShouldMove())
         {
             previouslyMoved = true;
             animationCompleted = 0f;
+            previousForwardDirection = body.forward;
+            otherFoot.previousForwardDirection = body.forward;
+            oldBodyPos = body.position;
+            otherFoot.oldBodyPos = oldBodyPos;
             FindHit(GetMovingFootRayCastPosition(stepLength));
-            enteringForward = body.forward;
+            //oldPosition = Quaternion.FromToRotation(previousForwardDirection, body.forward) * (oldPosition - body.position) + body.position;
             newPosition = hit.point;
             newNormal = hit.normal;
-            enteringBodyForward = body.forward;
         }
         if (animationCompleted < 1f)
         {
-            //if (!proceduralMovement.DetectedMovementInput())
-            //{
-            //    if(FindRecoveryHit())
-            //    {
-            //        transform.position = recoveryHit.point;
-            //        //animationCompleted = 0f;
-            //        //newPosition = recoveryHit.point;
-            //    }
-            //}
-            //else recoveryHit.point = Vector3.zero;
             AnimateStep();
             animationCompleted += !proceduralMovement.DetectedMovementInput() ? Time.deltaTime * recoverySpeed : Time.deltaTime * speed;
         }
@@ -112,15 +101,6 @@ public class IKFootSolver : MonoBehaviour
             footRotator = Quaternion.FromToRotation(Vector3.up, newNormal) * Quaternion.FromToRotation(Vector3.forward, body.forward);
             transform.rotation = footRotator * defaultToeRotation;
 
-            ////if body has changed it's forward vector (for example if you are heading up the slope, your feet will get
-            ////rotated in some way, but if you are coming down that slope again, you would want them to be rotated differently
-            ////slope will have different normal, but you will be heading in the opposite direction
-            //if (oldNormal != newNormal || Vector3.Angle(enteringBodyForward, body.forward) >= 90f)
-            //{
-                
-            //    //transform.rotation = Quaternion.LookRotation(body.forward, newNormal) * oldToeRotation;
-            //}
-            // leg has been moved - it can free other leg so it could move next
             if (FootMoved()) otherFoot.previouslyMoved = false;
             oldPosition = newPosition;
             oldNormal = newNormal;
@@ -140,7 +120,6 @@ public class IKFootSolver : MonoBehaviour
     {
         currentPosition = Vector3.Lerp(oldPosition, newPosition, animationCompleted);
         currentPosition.y += Mathf.Sin(animationCompleted * Mathf.PI) * stepHeight;
-        currentPosition = Quaternion.FromToRotation(enteringForward, body.forward) * (currentPosition - body.position) + body.position;
         currentNormal = Vector3.Lerp(oldNormal, newNormal, animationCompleted);
     }
     private void FindHit(Vector3 rayOrigin)
@@ -170,7 +149,7 @@ public class IKFootSolver : MonoBehaviour
     }
     private Vector3 GetStationaryFootRayCastPosition()
     {
-        return body.position + (body.right * footSpacing) + kneePosition;
+        return body.position + (body.right * footSpacing) + /*Quaternion.FromToRotation(Vector3.up, newNormal) **/ Vector3.up * kneeHeightOffset;
     }
 
     private Vector3 GetMovingFootRayCastPosition(float distanceLeft)
@@ -256,35 +235,37 @@ public class IKFootSolver : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        //Gizmos.color = Color.red;
+        Gizmos.color = Color.red;
 
-        //Gizmos.DrawCube(newPosition, new Vector3(sphereCastRadius, sphereCastRadius, sphereCastRadius));
+        Gizmos.DrawCube(newPosition, new Vector3(0.2f, 0.2f, 0.2f));
 
-        //Gizmos.color = Color.blue;
+        Gizmos.color = Color.blue;
 
-        //Gizmos.DrawCube(oldPosition, new Vector3(sphereCastRadius, sphereCastRadius, sphereCastRadius));
+        Gizmos.DrawCube(oldPosition, new Vector3(0.2f, 0.2f, 0.2f));
         //Gizmos.color = Color.green;
         //Gizmos.DrawLine(oldPosition, newPosition);
-        
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawSphere(recoveryHit.point, sphereCastRadius);
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(footTransform.position, sphereCastRadius);
-        Gizmos.color = Color.black;
 
-        CalculatePlaneParameters(body.right, Vector3.zero);
-        Quaternion rotationToPlane = Quaternion.FromToRotation(Vector3.right, intersectionPlane.normal);
-        Quaternion rotationToSlope = Quaternion.FromToRotation(Vector3.up, newNormal);
-        for (int i = 360; i >= 180; i--)
-        {
-            Vector3 pointOnPlane = CalculatePointOnPlane(i);
+        //Gizmos.color = Color.magenta;
+        //Gizmos.DrawSphere(recoveryHit.point, sphereCastRadius);
+        //Gizmos.color = Color.yellow;
+        //Gizmos.DrawSphere(footTransform.position, sphereCastRadius);
+        //Gizmos.color = Color.black;
 
-            // Rotate the point to align with the plane and move to world position
-            pointOnPlane = rotationToPlane * pointOnPlane;
-            pointOnPlane = rotationToSlope * pointOnPlane;
-            pointOnPlane += body.position + newNormal * chainLength;
-            Gizmos.DrawCube(pointOnPlane, new Vector3(sphereCastRadius, sphereCastRadius, sphereCastRadius));
-        }    
+        //CalculatePlaneParameters(body.right, Vector3.zero);
+        //Quaternion rotationToPlane = Quaternion.FromToRotation(Vector3.right, intersectionPlane.normal);
+        //Quaternion rotationToSlope = Quaternion.FromToRotation(Vector3.up, newNormal);
+        //for (int i = 360; i >= 180; i--)
+        //{
+        //    Vector3 pointOnPlane = CalculatePointOnPlane(i);
+
+        //    // Rotate the point to align with the plane and move to world position
+        //    pointOnPlane = rotationToPlane * pointOnPlane;
+        //    pointOnPlane = rotationToSlope * pointOnPlane;
+        //    pointOnPlane += body.position + newNormal * chainLength;
+        //    Gizmos.DrawCube(pointOnPlane, new Vector3(sphereCastRadius, sphereCastRadius, sphereCastRadius));
+        //}
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(currentPosition, sphereCastRadius);
         
     }
 }
